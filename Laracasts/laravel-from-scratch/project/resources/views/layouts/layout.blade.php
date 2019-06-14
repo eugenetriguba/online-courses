9c8c371c4918ce8d75223abd1ac1f4e8c5abf616
Eugene Triguba <eugenetriguba@gmail.com>
2019-06-14 13:34:55 -0500

    Create edit and update project actions

--- Laracasts/laravel-from-scratch/project/resources/views/layouts/layout.blade.php
+++ Laracasts/laravel-from-scratch/project/resources/views/layouts/layout.blade.php
@@ -5,9 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <title>@yield('title', 'App')</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css">
 </head>
 <body>
-    @yield('content')
+    <div class="container">
+        @yield('content')
+    </div>
 </body>
 </html>
---
+++ Laracasts/laravel-from-scratch/project/resources/views/projects/edit.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts/layout')
+
+@section('title', 'Edit a Project')
+
+@section('content')
+    <h1 class="title">Edit Project
+
+    <form action="/projects/{{ $project->id }}" method="POST">
+
+        @method('PATCH')
+        @csrf
+
+        <div class="field">
+            <label for="title" class="label">Title</label>
+        </div>
+
+        <div class="control">
+            <input type="text" class="input" name="title" placeholder="Title" value="{{ $project->title }}">
+        </div>
+
+        <div class="field">
+            <label for="description" class="label">Description</label>
+            
+            <div class="control">
+                <textarea name="description" id="" cols="30" rows="10" class="textarea">{{ $project->description }}</textarea>
+            </div>
+        </div>
+
+        <div class="field">
+            <div class="control">
+                <button class="button is-link" type="submit">Update Project</button>
+            </div>
+        </div>
+    </form>
+@endsection
