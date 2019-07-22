931f1944c565fb6706a371fcd227e29beaa061c9
Eugene Triguba <eugenetriguba@gmail.com>
2019-07-22 16:30:04 -0500

    Add acl lesson

---
+++ Laracasts/whats-new-in-laravel/acl-lesson/database/factories/PostFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\User;
+use App\Post;
+use Faker\Generator as Faker;
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'user_id' => factory(App\User::class)->create()->id,
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph
+    ];
+});
