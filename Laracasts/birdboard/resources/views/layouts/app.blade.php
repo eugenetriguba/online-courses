78486900546f430a5991da2ae35730cdf209f39d
Eugene Triguba <eugenetriguba@gmail.com>
2019-07-24 16:24:49 -0700

    Add a app layout and convert index over to it

---
+++ Laracasts/birdboard/resources/views/layouts/app.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Birdboard</title>
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
--- Laracasts/birdboard/resources/views/projects/index.blade.php
+++ Laracasts/birdboard/resources/views/projects/index.blade.php
@@ -1,15 +1,17 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Birdboard</title>
-    </head>
-    <body>
-        <h1>Birdboard</h1>
-        
-        <ul>
-            @foreach($projects as $project)
-            <li>{{ $project->title }}</li>
-            @endforeach
-        </ul>
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <h1>Birdboard</h1>
+
+    <ul>
+        @forelse($projects as $project)
+            <li>
+                <a href="{{ $project->path() }}">
+                    {{ $project->title }}
+                </a>
+            </li>
+        @empty
+            <li>No projects yet.</li>
+        @endforelse
+    </ul>
+@endsection
